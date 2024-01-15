db4ec006d2acbd7addcbab9fe7a69251162edc44
Aleksey Myakinin <amykis@mail.ru>
2024-01-15 09:29:50 +0300

    add task1 functions

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №1: Вычислить значение формулы (a x b) / (c  + d), где
+// a, b, c, d - некоторые целые числа.
+// Решение оформить в виде функции.
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double resalt = numenator / denumenator;
+    return resalt;
+}
+
+double result = CalculateFormula(1, 2, 3, 4);
+
+Console.WriteLine(result);
